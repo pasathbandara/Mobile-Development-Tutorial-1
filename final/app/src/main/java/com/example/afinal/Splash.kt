6dceb8035ef7bcb2a3f6e9ae235037247e901905
pasathbandara <101523910+pasathbandara@users.noreply.github.com>
2023-02-14 18:50:59 +0530

    more stuff added

---
+++ final/app/src/main/java/com/example/afinal/Splash.kt
@@ -0,0 +1,28 @@
+package com.example.afinal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val actionBtn = findViewById<Button>(R.id.readingbtn)
+        actionBtn.setOnClickListener(){
+            val Intent = Intent(this, MainActivity::class.java)
+            startActivity(Intent)
+            finish()
+
+
+        }
+
+
+
+
+    }
+}
